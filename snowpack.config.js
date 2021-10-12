ba2a931d53a24edf028d77379a442cd581ec66f9
Bryce Klinker <bryce.klinker@gmail.com>
2021-10-12 15:49:49 -0500

    Adding base set of code for getting github pages working for me

---
+++ snowpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mount: {
+        public: {url: '/', static: true},
+        src: {url: '/dist'}
+    },
+    plugins: [
+        '@snowpack/plugin-react-refresh',
+        '@snowpack/plugin-typescript'
+    ],
+    routes: [
+        {match: 'routes', src: '.*', dest: './index.html'}
+    ],
+    optimize: {
+        bundle: true
+    }
+}
